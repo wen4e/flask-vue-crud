7040b1b1876015387680ddf4e1072711daba4350
文思意 <912582340@qq.com>
2025-02-07 13:41:55 +0800

    添加 fundManagement 组件，包含基本模板、脚本和样式

---
+++ code/src/views/orgManage/fundManagement.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="fundManagement-container">
+    <h1>fundManagement</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fundManagement',
+  data() {
+    return {
+      // 数据属性
+    }
+  },
+  methods: {
+    // 方法定义
+  }
+}
+</script>
+
+<style scoped>
+.fundManagement-container {
+  padding: 20px;
+}
+</style>
