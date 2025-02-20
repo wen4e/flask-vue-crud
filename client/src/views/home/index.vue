97b60da89a1c4395ddc8ef5d051f0837bf12c614
文思意 <912582340@qq.com>
2025-02-20 17:13:57 +0800

    无代码更改

--- client/src/views/home/index.vue
+++ client/src/views/home/index.vue
@@ -1,52 +1,69 @@
 <template>
-  <vxe-table ref="tableRef" show-overflow :column-config="{ resizable: true }" :scroll-y="{ enabled: true, gt: 0 }" height="600" border stripe :loading="loading" :tree-config="{ transform: true, rowField: 'menuCode', parentField: 'uppMenuCode' }" :data="menuList" :checkbox-config="{ labelField: 'menuName', highlight: true }" :edit-config="{ trigger: 'manual', mode: 'row' }">
-    <vxe-column type="checkbox" title="菜单名称" tree-node width="320" fixed="left"></vxe-column>
-    <vxe-column field="menuCode" title="菜单码" :edit-render="{ name: 'VxeInput' }" width="auto"></vxe-column>
-    <vxe-column field="trCode" title="交易码" width="auto"></vxe-column>
-    <vxe-column field="uppMenuCode" title="上级菜单编码" width="auto"></vxe-column>
-    <vxe-column field="menuLevel" title="菜单级别" width="auto"></vxe-column>
-    <vxe-column field="menuKind" title="菜单分类" width="auto" :formatter="formatterMenuKind"></vxe-column>
-    <vxe-column field="menuVerify" title="权限校验" width="auto" :formatter="formatterMenuVerify"></vxe-column>
-    <vxe-column field="menuDisplay" title="菜单显示" width="auto" :formatter="formatterMenuDisplay"></vxe-column>
-    <vxe-column field="menuChecked" title="菜单选中" width="auto" :formatter="formatterMenuChecked"></vxe-column>
-    <vxe-column field="subsystemCode" title="系统编码" width="auto"></vxe-column>
-    <vxe-column field="folderCode" title="文件夹编码" width="auto"></vxe-column>
-    <vxe-column field="isAdmin" title="管理员是否可用" width="auto" :formatter="formatterFlag"></vxe-column>
-    <vxe-column field="isOperator" title="操作员是否可用" width="auto" :formatter="formatterFlag"></vxe-column>
-    <vxe-column field="menuAttribute" title="菜单属性" width="auto" :formatter="formatterMenuAttribute"></vxe-column>
-    <vxe-column field="sortNo" title="排序编号" width="auto"></vxe-column>
-    <vxe-column field="menuType" title="菜单类型" width="auto" :formatter="formatterMenuType"></vxe-column>
-    <vxe-column field="menuHerf" title="菜单链接" width="auto"></vxe-column>
-    <vxe-column field="workflowFlag" title="审批标志" width="auto" :formatter="formatterFlag"></vxe-column>
-    <vxe-column field="isKeepAlive" title="页面是否缓存" width="auto" :formatter="formatterFlag"></vxe-column>
-    <vxe-column title="操作" width="110" fixed="right">
-      <template #default="{ row }">
-        <template v-if="hasEditStatus(row)">
-          <el-tooltip effect="dark" content="保存" placement="top">
-            <el-icon class="mr-2 cursor-pointer" @click="saveRowEvent(row)"><Check /></el-icon>
-          </el-tooltip>
-          <el-tooltip effect="dark" content="取消" placement="top">
-            <el-icon class="mr-2 cursor-pointer" @click="cancelRowEvent()"><Close /></el-icon>
-          </el-tooltip>
+  <div>
+    <!-- 搜索输入框 -->
+    <vxe-input v-model="filterName" type="search" placeholder="搜索菜单" clearable @change="searchEvent" style="margin-bottom: 16px"></vxe-input>
+
+    <vxe-table ref="tableRef" show-overflow :column-config="{ resizable: true }" :scroll-y="{ enabled: true, gt: 0 }" height="600" border stripe :loading="loading" :tree-config="{ transform: true, rowField: 'menuCode', parentField: 'uppMenuCode' }" :data="menuList" :checkbox-config="{ labelField: 'menuName', highlight: true }" :edit-config="{ trigger: 'manual', mode: 'row' }">
+      <vxe-column type="checkbox" title="菜单名称" tree-node width="320" fixed="left"></vxe-column>
+      <vxe-column field="menuCode" title="菜单码" :edit-render="{ name: 'VxeInput' }" width="auto"></vxe-column>
+      <vxe-column field="trCode" title="交易码" width="auto"></vxe-column>
+      <vxe-column field="uppMenuCode" title="上级菜单编码" width="auto"></vxe-column>
+      <vxe-column field="menuLevel" title="菜单级别" width="auto"></vxe-column>
+      <vxe-column field="menuKind" title="菜单分类" width="auto" :formatter="formatterMenuKind"></vxe-column>
+      <vxe-column field="menuVerify" title="权限校验" width="auto" :formatter="formatterMenuVerify"></vxe-column>
+      <vxe-column field="menuDisplay" title="菜单显示" width="auto" :formatter="formatterMenuDisplay"></vxe-column>
+      <vxe-column field="menuChecked" title="菜单选中" width="auto" :formatter="formatterMenuChecked"></vxe-column>
+      <vxe-column field="subsystemCode" title="系统编码" width="auto"></vxe-column>
+      <vxe-column field="folderCode" title="文件夹编码" width="auto"></vxe-column>
+      <vxe-column field="isAdmin" title="管理员是否可用" width="auto" :formatter="formatterFlag"></vxe-column>
+      <vxe-column field="isOperator" title="操作员是否可用" width="auto" :formatter="formatterFlag"></vxe-column>
+      <vxe-column field="menuAttribute" title="菜单属性" width="auto" :formatter="formatterMenuAttribute"></vxe-column>
+      <vxe-column field="sortNo" title="排序编号" width="auto"></vxe-column>
+      <vxe-column field="menuType" title="菜单类型" width="auto" :formatter="formatterMenuType"></vxe-column>
+      <vxe-column field="menuHerf" title="菜单链接" width="auto"></vxe-column>
+      <vxe-column field="workflowFlag" title="审批标志" width="auto" :formatter="formatterFlag"></vxe-column>
+      <vxe-column field="isKeepAlive" title="页面是否缓存" width="auto" :formatter="formatterFlag"></vxe-column>
+      <vxe-column title="操作" width="110" fixed="right">
+        <template #default="{ row }">
+          <template v-if="hasEditStatus(row)">
+            <el-tooltip effect="dark" content="保存" placement="top">
+              <el-icon class="mr-2 cursor-pointer" @click="saveRowEvent(row)">
+                <Check />
+              </el-icon>
+            </el-tooltip>
+            <el-tooltip effect="dark" content="取消" placement="top">
+              <el-icon class="mr-2 cursor-pointer" @click="cancelRowEvent()">
+                <Close />
+              </el-icon>
+            </el-tooltip>
+          </template>
+          <template v-else>
+            <el-tooltip effect="dark" content="编辑" placement="top">
+              <el-icon class="mr-2 cursor-pointer" @click="editRowEvent(row)">
+                <Edit />
+              </el-icon>
+            </el-tooltip>
+            <el-tooltip effect="dark" content="复制" placement="top">
+              <el-icon class="mr-2 cursor-pointer" @click="copyRowEvent(row)">
+                <CopyDocument />
+              </el-icon>
+            </el-tooltip>
+            <el-tooltip effect="dark" content="删除" placement="top">
+              <el-icon class="mr-2 cursor-pointer" @click="delRowEvent(row)">
+                <Delete />
+              </el-icon>
+            </el-tooltip>
+            <el-tooltip effect="dark" content="生成页面" placement="top">
+              <el-icon class="cursor-pointer" @click="generatePageRowEvent(row)">
+                <Document />
+              </el-icon>
+            </el-tooltip>
+          </template>
         </template>
-        <template v-else>
-          <el-tooltip effect="dark" content="编辑" placement="top">
-            <el-icon class="mr-2 cursor-pointer" @click="editRowEvent(row)"><Edit /></el-icon>
-          </el-tooltip>
-          <el-tooltip effect="dark" content="复制" placement="top">
-            <el-icon class="mr-2 cursor-pointer" @click="copyRowEvent(row)"><CopyDocument /></el-icon>
-          </el-tooltip>
-          <el-tooltip effect="dark" content="删除" placement="top">
-            <el-icon class="mr-2 cursor-pointer" @click="delRowEvent(row)"><Delete /></el-icon>
-          </el-tooltip>
-          <el-tooltip effect="dark" content="生成页面" placement="top">
-            <el-icon class="cursor-pointer" @click="generatePageRowEvent(row)"><Document /></el-icon>
-          </el-tooltip>
-        </template>
-      </template>
-    </vxe-column>
-  </vxe-table>
-  <generate-page-dialog ref="generatePageDialogRef" />
+      </vxe-column>
+    </vxe-table>
+    <generate-page-dialog ref="generatePageDialogRef" />
+  </div>
 </template>
 
 <script setup>
@@ -57,10 +74,22 @@ import enums from "@/utils/menuCommon";
 import axios from "axios";
 import { useLocalStorage } from "@vueuse/core";
 import { ref, nextTick } from "vue";
-let SerialNo = "";
-// 表格配置
+import XEUtils from "xe-utils";
 
-// 表格格式化
+let SerialNo = "";
+
+// 新增变量：用于存储接口返回的完整菜单数据，用于搜索过滤
+let originalMenuList = [];
+
+// 搜索框绑定值
+const filterName = ref("");
+
+// 表格相关
+const loading = ref(false);
+const tableRef = ref();
+let menuList = ref([]);
+
+// 表格格式化函数
 const formatterMenuKind = ({ cellValue }) => {
   return enums.MENU_KIND_ENUM[cellValue];
 };
@@ -82,23 +111,20 @@ const formatterMenuType = ({ cellValue }) => {
 const formatterFlag = ({ cellValue }) => {
   return enums.ENABLE_ENUM[cellValue];
 };
-// 操作栏
-const loading = ref(false);
-const tableRef = ref();
+
+// 操作栏方法
 const hasEditStatus = (row) => {
   const $table = tableRef.value;
   if ($table) {
     return $table.isEditByRow(row);
   }
 };
-// 编辑
 const editRowEvent = (row) => {
   const $table = tableRef.value;
   if ($table) {
     $table.setEditRow(row);
   }
 };
-// 保存
 const saveRowEvent = (row) => {
   const $table = tableRef.value;
   if ($table) {
@@ -114,32 +140,50 @@ const saveRowEvent = (row) => {
     });
   }
 };
-// 取消编辑
 const cancelRowEvent = () => {
   const $table = tableRef.value;
   if ($table) {
     $table.clearEdit();
   }
 };
-// 复制
 const copyRowEvent = (row) => {
   const $table = tableRef.value;
   if ($table) {
+    // 示例: 复制当前行数据处理逻辑
   }
 };
-// 生成页面
-const generatePageDialogRef = ref(null); // 新增的 ref
+const generatePageDialogRef = ref(null);
 const generatePageRowEvent = (row) => {
   if (generatePageDialogRef.value) {
     generatePageDialogRef.value.show(row);
   }
 };
 
-// 登录
+// 登录方法
 const login = async () => {
   SerialNo = getRandomString(22);
   try {
-    const response = await axios.post("/tbspApi/tbsp/bank/tool/login", { headUserNo: "jres", headTrDate: "20250210", headSerialNo: SerialNo, headReqDate: "20250210", headReqTime: "193246", headReqSerialNo: SerialNo, headOrigDate: "20250210", headOrigTime: "193246", headOrigSerialNo: SerialNo, language: "1", orgNo: "", userNo: "jres", passwd: "33240f293bd2daad67ab8b1c6964b1b9", verificationCode: "", headChannel: "01", headOrgNo: "1", headCustNo: "000400000009999", headMenuCode: "bank", headTrCode: "tool" });
+    const response = await axios.post("/tbspApi/tbsp/bank/tool/login", {
+      headUserNo: "jres",
+      headTrDate: "20250210",
+      headSerialNo: SerialNo,
+      headReqDate: "20250210",
+      headReqTime: "193246",
+      headReqSerialNo: SerialNo,
+      headOrigDate: "20250210",
+      headOrigTime: "193246",
+      headOrigSerialNo: SerialNo,
+      language: "1",
+      orgNo: "",
+      userNo: "jres",
+      passwd: "33240f293bd2daad67ab8b1c6964b1b9",
+      verificationCode: "",
+      headChannel: "01",
+      headOrgNo: "1",
+      headCustNo: "000400000009999",
+      headMenuCode: "bank",
+      headTrCode: "tool",
+    });
     const loginInfo = response.data;
     useLocalStorage("loginInfo", loginInfo);
     if (response.data.respType === "S") {
@@ -150,23 +194,45 @@ const login = async () => {
   }
 };
 
-// 获取菜单列表
-let menuList = ref([]);
+// 获取菜单列表（接口返回数据同时存入 originalMenuList 供搜索使用）
 const getMenuList = async () => {
   SerialNo = getRandomString(22);
   try {
     const loginInfo = useLocalStorage("loginInfo", {});
-    const response = await axios.post("/tbspApi/tbsp/tool-pageMenu", { headUserNo: loginInfo.value.userId, headTrDate: "20250210", headSerialNo: SerialNo, headReqDate: "20250210", headReqTime: "195219", headReqSerialNo: SerialNo, headOrigDate: "20250210", headOrigTime: "195219", headOrigSerialNo: SerialNo, language: "1", menuScope: "4001", trCode: "", menuCode: null, menuName: null, isAdmin: "", isOperator: "", uppMenuCode: "bank-transactionBank", menuCodeLike: null, menuNameLike: null, copyClick: true, pager: false, headMenuCode: "tool-pageMenu" });
+    const response = await axios.post("/tbspApi/tbsp/tool-pageMenu", {
+      headUserNo: loginInfo.value.userId,
+      headTrDate: "20250210",
+      headSerialNo: SerialNo,
+      headReqDate: "20250210",
+      headReqTime: "195219",
+      headReqSerialNo: SerialNo,
+      headOrigDate: "20250210",
+      headOrigTime: "195219",
+      headOrigSerialNo: SerialNo,
+      language: "1",
+      menuScope: "4001",
+      trCode: "",
+      menuCode: null,
+      menuName: null,
+      isAdmin: "",
+      isOperator: "",
+      uppMenuCode: "bank-transactionBank",
+      menuCodeLike: null,
+      menuNameLike: null,
+      copyClick: true,
+      pager: false,
+      headMenuCode: "tool-pageMenu",
+    });
     if (response.data.respType === "S") {
+      // 将获取到的数据同时赋值给原始数据和当前显示数据
+      originalMenuList = response.data.dtos;
       menuList.value = response.data.dtos;
       nextTick(() => {
         const treeData = tableRef.value.getTableData();
         const firstLevelNodes = treeData.tableData;
         tableRef.value
           .setTreeExpand(firstLevelNodes, true)
-          .then(() => {
-            console.log("一级节点已展开");
-          })
+          .then(() => {})
           .catch((err) => {
             console.error("展开节点失败:", err);
           });
@@ -178,5 +244,32 @@ const getMenuList = async () => {
     console.error("Error fetching data:", error);
   }
 };
+
+// 搜索函数：根据 filterName 过滤菜单数据（示例中以 menuName 和 menuCode 字段作为匹配）
+const handleSearch = () => {
+  const filterVal = filterName.value.trim().toLowerCase();
+  if (filterVal) {
+    menuList.value = originalMenuList.filter((item) => {
+      return (item.menuName && item.menuName.toLowerCase().includes(filterVal)) || (item.menuCode && item.menuCode.toLowerCase().includes(filterVal)) || (item.trCode && item.trCode.toLowerCase().includes(filterVal)) || (item.uppMenuCode && item.uppMenuCode.toLowerCase().includes(filterVal));
+    });
+  } else {
+    menuList.value = originalMenuList;
+    nextTick(() => {
+      const treeData = tableRef.value.getTableData();
+      const firstLevelNodes = treeData.tableData;
+      tableRef.value
+        .setTreeExpand(firstLevelNodes, true)
+        .then(() => {})
+        .catch((err) => {
+          console.error("展开节点失败:", err);
+        });
+    });
+  }
+};
+
+// 节流搜索，避免频繁触发
+const searchEvent = XEUtils.throttle(handleSearch, 500, { trailing: true, leading: true });
+
+// 初始化加载菜单列表
 getMenuList();
 </script>
