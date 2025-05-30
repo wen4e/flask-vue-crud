bf75583016b4d8620fb4c8239ec94f53e9c2dfe2
文思意 <wensy51845@hundsun.com>
2025-05-30 10:00:26 +0800

    优化表格配置，添加行悬停效果，调整菜单列顺序并修复菜单链接和类型的显示

--- client/src/views/home/index.vue
+++ client/src/views/home/index.vue
@@ -32,6 +32,7 @@
     :height="tableHeight"
     :column-config="{ resizable: true }"
     :scroll-y="{ enabled: true, gt: 0 }"
+    :row-config="{ isHover: true }"
     border
     stripe
     :loading="loading"
@@ -45,11 +46,13 @@
   >
     <vxe-column type="checkbox" title="菜单名称" tree-node width="320" fixed="left"></vxe-column>
     <vxe-column field="menuCode" title="菜单码" width="auto"></vxe-column>
-    <vxe-column field="trCode" title="交易码" width="auto"></vxe-column>
     <vxe-column field="uppMenuCode" title="上级菜单码" width="auto"></vxe-column>
+    <vxe-column field="trCode" title="交易码" width="auto"></vxe-column>
+    <vxe-column field="menuHerf" title="菜单链接" width="200"></vxe-column>
+    <vxe-column field="menuType" title="菜单类型" width="auto" :formatter="formatterMenuType"></vxe-column>
+    <vxe-column field="menuVerify" title="权限校验" width="auto" :formatter="formatterMenuVerify"></vxe-column>
     <vxe-column field="menuLevel" title="菜单级别" width="auto"></vxe-column>
     <vxe-column field="menuKind" title="菜单分类" width="auto" :formatter="formatterMenuKind"></vxe-column>
-    <vxe-column field="menuVerify" title="权限校验" width="auto" :formatter="formatterMenuVerify"></vxe-column>
     <vxe-column field="menuDisplay" title="菜单显示" width="auto" :formatter="formatterMenuDisplay"></vxe-column>
     <vxe-column field="menuChecked" title="菜单选中" width="auto" :formatter="formatterMenuChecked"></vxe-column>
     <vxe-column field="subsystemCode" title="系统编码" width="auto"></vxe-column>
@@ -58,8 +61,6 @@
     <vxe-column field="isOperator" title="操作员是否可用" width="auto" :formatter="formatterFlag"></vxe-column>
     <vxe-column field="menuAttribute" title="菜单属性" width="auto" :formatter="formatterMenuAttribute"></vxe-column>
     <vxe-column field="sortNo" title="排序编号" width="auto"></vxe-column>
-    <vxe-column field="menuType" title="菜单类型" width="auto" :formatter="formatterMenuType"></vxe-column>
-    <vxe-column field="menuHerf" title="菜单链接" width="auto"></vxe-column>
     <vxe-column field="workflowFlag" title="审批标志" width="auto" :formatter="formatterFlag"></vxe-column>
     <vxe-column field="isKeepAlive" title="页面是否缓存" width="auto" :formatter="formatterFlag"></vxe-column>
     <vxe-column title="操作" width="110" fixed="right">
